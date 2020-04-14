8d5d0585d438ea7d275affa03b304cbe22a6a50a
longred <774931894@qq.com>
2020-04-14 16:13:16 +0800

    update
    Signed-off-by: longred <774931894@qq.com>

--- src/dataloader/Dataset.py
+++ src/dataloader/Dataset.py
@@ -2,13 +2,16 @@ from torchtext.data import BucketIterator, Dataset, Example, Field
 from src.dataloader.utils import load_data
 from src.util.utils import search
 import json
+from src.util.tokenizers import Tokenizer
 
 max_length = 512
 
 
 def get_data_info(event_schema_path):
     with open(event_schema_path, encoding="UTF-8") as f:
-        id2label, label2id, n = {}, {}, 0
+        id2label, label2id, n = {}, {}, 1
+        id2label.update({0:'padding'})
+        label2id.update({'padding':0})
         for l in f:
             l = json.loads(l)
             for role in l['role_list']:
@@ -16,7 +19,9 @@ def get_data_info(event_schema_path):
                 id2label[n] = key
                 label2id[key] = n
                 n += 1
-        num_labels = len(id2label) *2 + 1
+        id2label.update({n:'O'})
+        label2id.update({'O':n})
+        num_labels = len(id2label)
     return num_labels, id2label, label2id
 
 
@@ -24,19 +29,33 @@ class EETaskDataset(Dataset):
     def __init__(self, path, fields, tokenizer, label2id):
         examples = []
         data = load_data(path)
-        token_ids_list, labels_list = [], []
+        O = label2id['O']
         for (text, arguments) in data:
-            token_ids = tokenizer.encode(text, max_length=max_length)[0]
-            labels = [0] * len(token_ids)
+            input_ids, token_type_ids = tokenizer.encode(
+                text, max_length=max_length)
+
+            seq_len = len(input_ids)
+            labels = [O] * seq_len
+            labels[0] = label2id['padding']
+            labels[-1] = label2id['padding']
+            attention_mask = [1]*seq_len
             for argument in arguments.items():
                 a_token_ids = tokenizer.encode(argument[0])[0][1:-1]
-                start_index = search(a_token_ids, token_ids)
+                start_index = search(a_token_ids, input_ids)
                 if start_index != -1:
-                    labels[start_index] = label2id[argument[1]] * 2+ 1
-                    for i in range(1, len(a_token_ids)):
-                        labels[start_index + i] = label2id[argument[1]] * 2+ 2
+                    for i in range(0, len(a_token_ids)):
+                        labels[start_index + i] = label2id[argument[1]]
+                # if start_index != -1:
+                #     labels[start_index] = label2id[argument[1]] * 2+ 1
+                #     for i in range(1, len(a_token_ids)):
+                #         labels[start_index + i] = label2id[argument[1]] * 2+ 2
+
+
+            assert len(input_ids) == len(token_type_ids) == len(
+                attention_mask) == len(labels) == seq_len
+
             examples.append(Example.fromlist([
-                token_ids, labels, len(token_ids)
+                input_ids, token_type_ids, attention_mask, labels, seq_len
             ], fields))
         super().__init__(examples, fields)
 
@@ -44,7 +63,7 @@ class EETaskDataset(Dataset):
 class EETaskDataloader:
     def __init__(self, config):
         self.config = config
-        from src.util.tokenizers import Tokenizer
+
 
         self.tokenizer = Tokenizer(self.config.vocab_path)
 
@@ -52,19 +71,22 @@ class EETaskDataloader:
         from torchtext.data import RawField
         from src.dataloader.utils import sequence_padding
         self.fields = [
-            ("input", RawField(postprocessing=sequence_padding)),
-            ("target", RawField(postprocessing=sequence_padding)),
-            ("len", RawField())
+            ("input_ids", RawField(postprocessing=sequence_padding)),
+            ("token_type_ids", RawField(postprocessing=sequence_padding)),
+            ("attention_mask", RawField(postprocessing=sequence_padding)),
+            ("label_ids", RawField(postprocessing=sequence_padding)),
+            ("seq_len", RawField())
         ]
 
     def get_train_data_loader(self):
         self.traindataset = EETaskDataset(self.config.train_data_path, self.fields, self.tokenizer, self.label2id)
         self.trainloader = BucketIterator(self.traindataset, batch_size=self.config.batch_size,
-                                          sort_key=lambda x: x.len,
+                                          sort_key=lambda x: x.seq_len,
                                           sort=True,
                                           sort_within_batch=True, train=True, shuffle=True)
         return self.trainloader
 
+#%%
 if __name__ == '__main__':
     config = classmethod(-1)
     config.vocab_path = r"\\wsl$\Ubuntu\home\longred\EETask\prev_trained_model\albert_tiny_zh\vocab.txt"
@@ -79,3 +101,5 @@ if __name__ == '__main__':
     #%%
     next(t)
 
+
+# %%
